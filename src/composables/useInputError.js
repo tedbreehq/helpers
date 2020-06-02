704a9b3b63156e793a21f3a291d63dc1f63d27dc
Dami Ogunmoye <dami@ogunmoye.com>
2020-06-02 01:09:42 +0100

    add useInputerror

---
+++ src/composables/useInputError.js
@@ -0,0 +1,28 @@
+import { isArray } from '../helpers';
+import { ref, computed } from "@vue/composition-api";
+
+
+export const useInputError = () => {
+
+    const error = ref(null);
+
+
+    const hasError = computed(() => {
+        if (isArray(error)) {
+            return error.length > 0;
+        }
+
+        return error !== null;
+    });
+
+
+    const getError = computed(() => {
+        if (isArray(error)) {
+            return error[0];
+        }
+
+        return error;
+    });
+
+    return  { error, hasError, getError };
+}
